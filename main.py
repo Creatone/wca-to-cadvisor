7e4c26a86b9fc969448822e3216bb8a6f4fccbd2
Paweł Szulik <pawel.szulik@intel.com>
2020-06-22 12:09:05 +0200

    Generate perf configs.
    Signed-off-by: Paweł Szulik <pawel.szulik@intel.com>

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
+import json
+
 from wca.perf_const import PREDEFINED_RAW_EVENTS
 from wca.platforms import CPUCodeName
-import json
 
 cadvisor_perf_config = {
     "events": [],
     "custom_events": []
 }
 
-code_name = CPUCodeName.SKYLAKE
+CODE_NAMES = [code_name
+              for code_name in CPUCodeName
+              if code_name != CPUCodeName.UNKNOWN]
 
-for event in PREDEFINED_RAW_EVENTS:
-    if code_name in PREDEFINED_RAW_EVENTS[event]:
-        cadvisor_perf_config["events"].append([event])
-        cadvisor_perf_config["custom_events"].append({
-            "type": 4,
-            "config": list(
-                hex(code) for code in
-                PREDEFINED_RAW_EVENTS[event][code_name]),
-            "name": event
-        })
-
-with open("perf-wca.json", "w") as f:
-    f.write(json.dumps(cadvisor_perf_config))
-
+for code_name in CODE_NAMES:
+    for event in PREDEFINED_RAW_EVENTS:
+        if code_name in PREDEFINED_RAW_EVENTS[event]:
+            cadvisor_perf_config["events"].append([event])
+            cadvisor_perf_config["custom_events"].append({
+                "type": 4,
+                "config": list(
+                    hex(code) for code in
+                    PREDEFINED_RAW_EVENTS[event][code_name]),
+                "name": event
+            })
+    with open("perf-wca-%s" % code_name.value, "w") as f:
+        f.write(json.dumps(cadvisor_perf_config))
